4dae6a680617e0fb124c76b60dc546eb80fbe3ea
notthereal_Sam <sam.sabin97@gmail.com>
2026-03-13 00:32:12 -0400

    updated arduino code to work with BTS7960 motor controller instead

--- Arduino/arduino_listner/arduino_listner.ino
+++ Arduino/arduino_listner/arduino_listner.ino
@@ -1,16 +1,32 @@
-const int IN1 = 8;
-const int IN2 = 9;
-const int ENA = 5;  // must be PWM pin
+const int R_EN = 8;
+const int L_EN = 9;
+const int RPWM = 5;
+const int LPWM = 6;  // must be PWM pin
 
 String input = "";
 
 void setup() {
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(ENA, OUTPUT);
+  // had to redo code since using a new motor controller
+  // pinMode(IN1, OUTPUT);
+  // pinMode(IN2, OUTPUT);
+  // pinMode(RPWM, OUTPUT);
+  // pinMode(LED_BUILTIN, OUTPUT);
+  // Serial.begin(9600);
+  pinMode(R_EN, OUTPUT);
+  pinMode(L_EN, OUTPUT);
+  pinMode(RPWM, OUTPUT);
+  pinMode(LPWM, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
+  digitalWrite(R_EN, HIGH);
+  digitalWrite(L_EN, HIGH);
+
+  analogWrite(RPWM, 0);
+  analogWrite(LPWM, 0);
+
+  delay(1000);
   Serial.begin(9600);
+
 }
 
 void loop() {
@@ -30,21 +46,27 @@ void handleCommand(String cmd) {
   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); // debug blink
 
   if (cmd == "FWD") {
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
-    analogWrite(ENA, 200);
+    // digitalWrite(IN1, HIGH);
+    // digitalWrite(IN2, LOW);
+    // analogWrite(RPWM, 200);
+    analogWrite(RPWM, 200);
+    analogWrite(LPWM, 0);
   }
 
   else if (cmd == "REV") {
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, HIGH);
-    analogWrite(ENA, 200);
+    // digitalWrite(IN1, LOW);
+    // digitalWrite(IN2, HIGH);
+    // analogWrite(RPWM, 200);
+    analogWrite(RPWM, 0);
+    analogWrite(LPWM, 200);
   }
 
   else if (cmd == "STOP") {
-    analogWrite(ENA, 0);
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, LOW);
+    // analogWrite(RPWM, 0);
+    // digitalWrite(IN1, LOW);
+    // digitalWrite(IN2, LOW);
+    analogWrite(RPWM, 0);
+    analogWrite(LPWM, 0);
   }
 }
 
